a4a7a575ee56894ee726931527111c62bd78c14c
Andrew Hu <andrewhu32009@gmail.com>
2025-05-15 01:01:02 -0400

    chore: 08 removefromArray

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    // test 1
+    // for (let i = 0; i < arr.length; i++) {
+    //     if (arr[i] === num) {
+    //         arr.splice(i, 1);
+    //     }
+    // }
+
+    
+    return arr.filter((val) => {return !args.includes(val)} );
 };
 
 // Do not edit below this line
