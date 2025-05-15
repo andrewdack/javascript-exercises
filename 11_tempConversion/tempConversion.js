f3f1119948c7eb07982d44f1cb4838c8446c7f9b
Andrew Hu <andrewhu32009@gmail.com>
2025-05-15 01:29:08 -0400

    chore: 11 tempConversion

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degreesInFahrenheit) {
+  let conversionDecimal = (5/9) * (degreesInFahrenheit - 32);
+  return Number(conversionDecimal.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degreesInCelsius) {
+  let conversionDecimal = (degreesInCelsius * (9/5)) + 32;
+  return Number(conversionDecimal.toFixed(1));
 };
 
 // Do not edit below this line
