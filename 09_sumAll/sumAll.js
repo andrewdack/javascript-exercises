0724a270ae870ab6788d1ea5e15f7674c0702d41
Andrew Hu <andrewhu32009@gmail.com>
2025-05-15 01:10:36 -0400

    chore: 09 sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    // guard clauses against non-acceptable parameters
+    if (num1 < 0 || num2 < 0) {
+        return "ERROR";
+    }
+    else if (!Number.isInteger(num1) || !Number.isInteger(num2)) {
+        return "ERROR";
+    }
+    
+    let startNum = Math.min(num1, num2);
+    let endNum = (startNum === num1) ? num2 : num1;
 
+    let sum = 0;
+    for (let num = startNum; num <= endNum; num++) {
+        sum += num;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
