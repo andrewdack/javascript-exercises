edfdc9997dae7a6e3354e180b848b32796d33568
Andrew Hu <andrewhu32009@gmail.com>
2025-05-15 00:46:02 -0400

    chore: 07 reverseString

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let res = "";
 
+    for (let i = str.length - 1; i >= 0; i--) {
+        res += str.slice(i, i + 1);
+    }
+    return res;
 };
 
 // Do not edit below this line
